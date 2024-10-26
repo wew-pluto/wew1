ada60e9a481836dd70db2fda6a7a222a3130fc53
wew-pluto <wew122333444455555@gmail.com>
2024-10-26 16:06:01 +0300

    Create Функции: проектная работа, оптимизация игры.C.py

---
+++ Функции: проектная работа, оптимизация игры.C.py
@@ -0,0 +1,18 @@
+import random
+def dice(ta=True):
+ tc=0
+ if ta==True:
+  print("Сколько кубиков хотите бросить?")
+  ta=float(input())
+ if ta>=1 and ta<=3 and ta==ta//1:
+  print(int(ta))
+  print("Вы бросаете "+str(int(ta))+" кубиков")
+  for tq in range(int(ta)):
+   tb=random.randint(1,6)
+   tc+=tb
+   print("Кубик "+str(tq+1)+": "+str(tb))
+   print("Суммарный результат: "+str(tc))
+ else: 
+  print("Нельзя бросить столько кубиков, попробуйте снова")
+#  dice(ta)
+dice()
